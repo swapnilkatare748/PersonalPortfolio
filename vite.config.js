7c70c7c03b3916630d4915436c50bdc460bf3af7
swapnil katare <swapnilkatare748@gmail.com>
2024-12-09 16:33:16 +0530

    vercel apdated the vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // This ensures the paths are correctly resolved during deployment.
   build: {
-    outDir: 'dist', // Output directory for production build
+    outDir: 'dist', // Build output directory
   },
+  publicDir: 'public', // Directory for static assets
 });
