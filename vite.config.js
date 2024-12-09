239f215e70f11e215976f8d41a1e5b1e7993a8b8
swapnil katare <swapnilkatare748@gmail.com>
2024-12-09 16:48:17 +0530

    chenge in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // Build output directory
-  },
-  publicDir: 'public', // Directory for static assets
-});
+  plugins: [react()],
+})
+
+
